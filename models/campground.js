75d6d0e083971c52d216655d522bd2db52452722
Nam Anh <namanh022@gmail.com>
2022-01-06 22:51:45 +0200

    add authentication and authorization

--- models/campground.js
+++ models/campground.js
@@ -8,11 +8,14 @@ const CampgroundSchema = new Schema({
     price: Number,
     description: String,
     location: String,
+    author:
+    {
+        type: Schema.Types.ObjectId, ref : 'User'  
+    },
     reviews :[
-        {
-            type: Schema.Types.ObjectId, ref: 'Review' 
-        }
-    ]
+    {
+        type: Schema.Types.ObjectId, ref: 'Review' 
+    }]
 });
 
 //--- middleware of deleting all the reviews associated with campgrounds when deleting campgrounds
