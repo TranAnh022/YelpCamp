5bb856b6cfc2ce50e9d21aeb0083f16cb94b81a0
Nam Anh <namanh022@gmail.com>
2022-01-19 15:39:25 +0200

    Add cloudinary

---
+++ cloudinary/index.js
@@ -0,0 +1,22 @@
+
+//-- connect to cloudinary--
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+
+cloudinary.config({                           //setting cofiguration parameters globally
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET
+});
+
+const storage = new CloudinaryStorage({
+    cloudinary,
+    params: {
+        folder: 'YelpCamp',                     // create a floder in cloudinary
+        allowedFormats: ['jpeg', 'png', 'jpg']
+    }
+})
+
+module.exports = {
+    cloudinary,storage
+}
